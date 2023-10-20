bcac53cf8143224257593985e95323c9def6d976
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-10-20 15:46:47 -0500

    Add placeholder content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-library
+
+Assignment for [The Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-library).
---
+++ app.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(author, title, pages, isRead = false) {
+  this.author = author;
+  this.title = title;
+  this.pages = numPages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayLibrary() {
+  const main = document.getElementById("main");
+}
+
+function createBookCard(book) {
+  const card = document.createElement("div");
+}
